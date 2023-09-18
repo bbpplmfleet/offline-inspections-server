56371f97cfd1fbffd71e8f6e2cfda9020f04da65
Brendan Probst <bprobst@plmfleet.com>
2023-09-18 10:21:50 -0400

    adjusting cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:4173");
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://offline-inspections-client.vercel.app/**"
+    "https://offline-inspections-client.vercel.app"
   );
   // res.setHeader(
   //   "Access-Control-Allow-Origin",
